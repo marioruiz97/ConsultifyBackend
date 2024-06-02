c9223d05f29f0bccc4040cb7ece968b9b16616e2
Mario Ruiz <marioarb.97@gmail.com>
2024-06-02 00:22:53 -0500

    cambios en ControladorNotificacion

--- src/main/java/com/asisge/consultifybackend/notificaciones/infraestructura/controlador/ControladorNotificacion.java
+++ src/main/java/com/asisge/consultifybackend/notificaciones/infraestructura/controlador/ControladorNotificacion.java
@@ -3,18 +3,20 @@ package com.asisge.consultifybackend.notificaciones.infraestructura.controlador;
 import com.asisge.consultifybackend.notificaciones.aplicacion.servicio.ServicioNotificacion;
 import com.asisge.consultifybackend.notificaciones.dominio.modelo.Notificacion;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping
-public class ContoladorNotificacion {
+@RequestMapping("/notificaciones")
+@PreAuthorize("isAuthenticated()")
+public class ControladorNotificacion {
 
     private final ServicioNotificacion servicioNotificacion;
 
     @Autowired
-    public ContoladorNotificacion(ServicioNotificacion servicioNotificacion) {
+    public ControladorNotificacion(ServicioNotificacion servicioNotificacion) {
         this.servicioNotificacion = servicioNotificacion;
     }
 
